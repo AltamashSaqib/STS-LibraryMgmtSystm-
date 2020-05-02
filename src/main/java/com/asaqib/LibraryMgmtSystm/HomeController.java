4a16e8ce81356a6ae1f04aece524ec710f04a905
Altamash Saqib <asaqib@altimetrik.com>
2020-05-02 14:15:50 +0530

    Todays Commit

--- src/main/java/com/asaqib/LibraryMgmtSystm/HomeController.java
+++ src/main/java/com/asaqib/LibraryMgmtSystm/HomeController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.asaqib.LibraryMgmtSystm.model.Books;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200")
 public class HomeController {	
 	@Autowired
 	AccRepo repo;
